28fc00edbc92ba55fe00e2136c7fd7df9ff4f92c
kapumota <claraa@uni.edu.pe>
2026-06-10 00:09:49 -0500

    fase 7: corrige formato Rust

--- rust/input-guard-grpc/build.rs
+++ rust/input-guard-grpc/build.rs
@@ -2,6 +2,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .compile_protos(&["../../proto/validator/v1/validator.proto"], &["../../proto"])?;
+        .compile_protos(
+            &["../../proto/validator/v1/validator.proto"],
+            &["../../proto"],
+        )?;
     Ok(())
 }
